c227e40178fba9b3e31489c993ad03db0c2f0f06
Miguel <mmirandasanchez16@gmail.com>
2025-03-10 14:59:43 -0300

    add .env to gitignore

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,23 +1,27 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const adminAuth = (req, res, next) =>{
-    try {
-        const {token} = req.headers;
-        if(!token){
-            return res.status(401).json({success: false, message: "Not authenticated Login again"})
-        }
-
-        const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        if(tokenDecoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD ){
-            return res.status(401).json({success: false, message: "Not authenticated Login again"})
-        }
-
-        next(); 
-    } catch (error) {
-        console.log(error.message)
-        res.status(409).json({success:false, message: error.message})
+const adminAuth = (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not JWT Login Again" });
     }
-}
+
+    const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("token", tokenDecoded);
+    if (tokenDecoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not authenticated Login again" });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res.status(409).json({ success: false, message: error.message });
+  }
+};
 
 export default adminAuth;
