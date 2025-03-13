292dc4a74b72784c64bd5637e07035692b7c39f3
Miguel <mmirandasanchez16@gmail.com>
2025-03-13 12:18:27 -0300

    handle module import error in controllers

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -9,7 +9,7 @@ import {
   updateStatus,
   verifyStripe,
 } from "../controllers/orderController.js";
-import adminAuth from "../middleware/adminauth.js";
+import adminAuth from "../middleware/adminAuth.js";
 import authUser from "../middleware/auth.js";
 
 const orderRouter = Router();
